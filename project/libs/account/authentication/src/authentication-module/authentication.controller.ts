5c92ec68b61422123cd2fe89de492e61f31bf4ee
Anton Paskanny <anton.paskanny@gmail.com>
2024-04-03 23:11:18 +0300

    feat: add signin and id routes

--- project/libs/account/authentication/src/authentication-module/authentication.controller.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.controller.ts
@@ -1,15 +1,28 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param } from '@nestjs/common';
 
 import { AuthenticationService } from './authentication.service';
 import { SignUpUserDto } from '../dto/signup-user.dto';
+import { SignInUserDto } from '../dto/signin-user.dto';
 
 @Controller('auth')
 export class AuthenticationController {
   constructor(private readonly authService: AuthenticationService) {}
 
   @Post('signup')
-  public async create(@Body() dto: SignUpUserDto) {
+  public async signup(@Body() dto: SignUpUserDto) {
     const newUser = await this.authService.register(dto);
     return newUser.toPOJO();
+  }
+
+  @Post('sigin')
+  public async signin(@Body() dto: SignInUserDto) {
+    const verifiedUser = await this.authService.verifyUser(dto);
+    return verifiedUser.toPOJO();
+  }
+
+  @Get(':id')
+  public async show(@Param('id') id: string) {
+    const existUser = await this.authService.getUserById(id);
+    return existUser.toPOJO();
   }
 }
