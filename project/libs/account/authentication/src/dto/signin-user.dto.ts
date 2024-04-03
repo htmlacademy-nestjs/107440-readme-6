3e1051babcc9fb5bbf0e22919a92b9b98fd32078
Anton Paskanny <anton.paskanny@gmail.com>
2024-04-03 22:31:48 +0300

    feat: add user dtos

---
+++ project/libs/account/authentication/src/dto/signin-user.dto.ts
@@ -0,0 +1,4 @@
+export class SignInUserDto {
+  public email: string;
+  public password: string;
+}
---
+++ project/libs/account/authentication/src/dto/signup-user.dto.ts
@@ -0,0 +1,7 @@
+export class SignUpUserDto {
+  public email: string;
+  public dateBirth: string;
+  public firstname: string;
+  public lastname: string;
+  public password: string;
+}
