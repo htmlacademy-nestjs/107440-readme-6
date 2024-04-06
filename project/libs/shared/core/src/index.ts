a1c66cf983778cb63b312665508f3bd30fd29e22
Anton Paskanny <anton.paskanny@gmail.com>
2024-04-06 14:27:43 +0300

    feat: update shared/core lib

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -4,5 +4,13 @@ export { UserRole } from './lib/types/user-role.enum';
 export { User } from './lib/types/user.interface';
 export { AuthUser } from './lib/types/auth-user.interface';
 
+export { PostType } from './lib/types/posts/post-type.enum';
+export { PostState } from './lib/types/posts/post-state.enum';
+export { PostVideo } from './lib/types/posts/post-video.interface';
+export { PostText } from './lib/types/posts/post-text.interface';
+export { PostQuote } from './lib/types/posts/post-quote.interface';
+export { PostLink } from './lib/types/posts/post-link.interface';
+export { PostPhoto } from './lib/types/posts/post-photo.interface';
+
 export { StorableEntity } from './lib/interfaces/storable-entity.interface';
 export { EntityFactory } from './lib/interfaces/entity-factory.interface';
