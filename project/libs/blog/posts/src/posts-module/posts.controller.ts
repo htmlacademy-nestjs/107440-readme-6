664ba28a53e0571d7872f1dab2355659f136d9f9
Anton Paskanny <anton.paskanny@gmail.com>
2024-04-12 11:53:26 +0300

    feat: update posts controller

--- project/libs/blog/posts/src/posts-module/posts.controller.ts
+++ project/libs/blog/posts/src/posts-module/posts.controller.ts
@@ -7,17 +7,29 @@ import {
   Put,
   Get,
   Body,
+  HttpStatus,
 } from '@nestjs/common';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 import { BlogPostDto } from '../dto';
 import { BlogPostRepository } from '../repositories/post.repository';
 import { BlogPostService } from './posts.service';
+import { PostsResponseMessage } from './posts.constant';
 
+@ApiTags('posts')
 @Controller('posts')
 export class BlogPostController {
   constructor(
     private blogPostRepository: BlogPostRepository,
     private blogPostsService: BlogPostService
   ) {}
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: PostsResponseMessage.PostsFound,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: PostsResponseMessage.PostsNotFound,
+  })
   @Get()
   gestPosts(
     @Query('offset') offset: number,
@@ -28,32 +40,76 @@ export class BlogPostController {
     // Implementation
   }
 
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: PostsResponseMessage.PostCreated,
+  })
   @Post()
   createPost(@Body() blogPostDto: BlogPostDto) {
     const blogPost = this.blogPostsService.createPost(blogPostDto);
     return blogPost;
   }
 
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: PostsResponseMessage.PostUpdated,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: PostsResponseMessage.PostNotFound,
+  })
   @Put('/:postId')
   updatePost(@Param('postId') postId: string) {
     // Implementation
   }
 
+  @ApiResponse({
+    status: HttpStatus.NO_CONTENT,
+    description: PostsResponseMessage.PostDeleted,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: PostsResponseMessage.PostNotFound,
+  })
   @Delete('/:postId')
   deletePost(@Param('postId') postId: string) {
     // Implementation
   }
 
+  @ApiResponse({
+    status: HttpStatus.NO_CONTENT,
+    description: PostsResponseMessage.PostLiked,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: PostsResponseMessage.PostNotFound,
+  })
   @Post('/:postId/like')
   addLikeToPost(@Param('postId') postId: string) {
     // Implementation
   }
 
+  @ApiResponse({
+    status: HttpStatus.NO_CONTENT,
+    description: PostsResponseMessage.PostUnliked,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: PostsResponseMessage.PostNotFound,
+  })
   @Delete('/:postId/like')
   deleteLikeFromPost(@Param('postId') postId: string) {
     // Implementation
   }
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: PostsResponseMessage.PostsFound,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: PostsResponseMessage.PostsNotFound,
+  })
   @Get('/user/:userId')
   getPostsByUserId(
     @Param('userId') userId: string,
@@ -63,6 +119,14 @@ export class BlogPostController {
     // Implementation
   }
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: PostsResponseMessage.PostsFound,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: PostsResponseMessage.PostsNotFound,
+  })
   @Get('/search')
   searchPostsByTitle(@Query('title') title: string) {
     // Implementation
