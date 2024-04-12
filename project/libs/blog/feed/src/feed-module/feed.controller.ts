30fb3a217624e2b78048241325ed00252778d7f3
Anton Paskanny <anton.paskanny@gmail.com>
2024-04-12 12:09:21 +0300

    feat: update feed controller

--- project/libs/blog/feed/src/feed-module/feed.controller.ts
+++ project/libs/blog/feed/src/feed-module/feed.controller.ts
@@ -1,17 +1,47 @@
-import { Controller, Post, Delete, Param, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Delete,
+  Param,
+  Get,
+  HttpStatus,
+} from '@nestjs/common';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
+import { FeedResponseMessage } from './feed.constant';
 
+@ApiTags('feed')
 @Controller('feed')
 export class FeedController {
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: FeedResponseMessage.FeedIsReady,
+  })
   @Get('/')
   getFeed() {
     // Implementation
   }
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: FeedResponseMessage.Subscribe,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: FeedResponseMessage.UserNotFound,
+  })
   @Post('/subscribe/:userId')
   subscribeToUser(@Param('postId') postId: string) {
     // Implementation
   }
 
+  @ApiResponse({
+    status: HttpStatus.NO_CONTENT,
+    description: FeedResponseMessage.Unsubscribe,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: FeedResponseMessage.UserNotFound,
+  })
   @Delete('/subscribe/:userId')
   unsubscribeFromUser(@Param('userId') userId: string) {
     // Implementation
