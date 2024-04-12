394286bd141a42d5d860394cb44f544ebe281208
Anton Paskanny <anton.paskanny@gmail.com>
2024-04-12 12:04:34 +0300

    feat: update comments controller

---
+++ project/libs/blog/comments/src/comments-module/comment.constant.ts
@@ -0,0 +1,6 @@
+export const CommentsResponseMessage = {
+  CommentsFound: 'Comments found',
+  PostNotFound: 'Post not found',
+  PostFound: 'Post found',
+  PostOrCommentNotFound: 'Post or Comment not found',
+} as const;
