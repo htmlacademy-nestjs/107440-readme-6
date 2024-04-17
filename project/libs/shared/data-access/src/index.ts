b6910b5fb717d2669de5012dd7d69006b91ede2f
Anton Paskanny <anton.paskanny@gmail.com>
2024-04-17 18:33:17 +0300

    feat: add postgres repository

--- project/libs/shared/data-access/src/index.ts
+++ project/libs/shared/data-access/src/index.ts
@@ -2,3 +2,5 @@ export { BaseMemoryRepository } from './repository/base-memory.repository';
 export { Repository } from './repository/repository.interface';
 
 export { BaseMongoRepository } from './repository/base-mongo.repository';
+
+export { BasePostgresRepository } from './repository/base-postgres.repository';
