f7c917a61d939541cc3d8b5ca83b48ae38ec5237
Anton Paskanny <anton.paskanny@gmail.com>
2024-04-20 15:28:22 +0300

    feat: update Post and VideoPost repositories.

--- project/libs/blog/posts/src/repositories/post-video.repository.ts
+++ project/libs/blog/posts/src/repositories/post-video.repository.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { BaseMemoryRepository } from '@project/data-access';
+import { BasePostgresRepository } from '@project/data-access';
+import { PostVideo } from '@project/core';
+import { PrismaClientService } from '@project/blog-models';
 
 import { PostVideoFactory } from '../factories/post-video.factory';
 import { PostVideoEntity } from '../entities/post.video.entity';
 
 @Injectable()
-export class PostVideoRepository extends BaseMemoryRepository<PostVideoEntity> {
-  constructor(entityFactory: PostVideoFactory) {
-    super(entityFactory);
+export class PostVideoRepository extends BasePostgresRepository<
+  PostVideoEntity,
+  PostVideo
+> {
+  constructor(
+    entityFactory: PostVideoFactory,
+    readonly client: PrismaClientService
+  ) {
+    super(entityFactory, client);
+  }
+
+  public async save(entity: PostVideoEntity): Promise<void> {
+    const record = await this.client.videoPost.create({
+      data: { ...entity.toPOJO() },
+    });
+
+    entity.id = record.id;
   }
 }
--- project/libs/blog/posts/src/repositories/post.repository.ts
+++ project/libs/blog/posts/src/repositories/post.repository.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { BaseMemoryRepository } from '@project/data-access';
+import { BasePostgresRepository } from '@project/data-access';
+import { PrismaClientService } from '@project/blog-models';
+
 import { BlogPostFactory } from '../factories';
 import { BlogPostEntity } from '../entities';
+import { BlogPost } from '@project/core';
 
 @Injectable()
-export class BlogPostRepository extends BaseMemoryRepository<BlogPostEntity> {
-  constructor(entityFactory: BlogPostFactory) {
-    super(entityFactory);
+export class BlogPostRepository extends BasePostgresRepository<
+  BlogPostEntity,
+  BlogPost
+> {
+  constructor(
+    entityFactory: BlogPostFactory,
+    readonly client: PrismaClientService
+  ) {
+    super(entityFactory, client);
+  }
+
+  public async save(entity: BlogPostEntity): Promise<void> {
+    const record = await this.client.post.create({
+      data: { ...entity.toPOJO() },
+    });
+
+    entity.id = record.id;
   }
 }
