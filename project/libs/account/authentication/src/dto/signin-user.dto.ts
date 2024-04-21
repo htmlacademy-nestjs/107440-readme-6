3eba6ad608cce2b767a5e2faf8f66500f6ea02ac
Anton Paskanny <anton.paskanny@gmail.com>
2024-04-21 11:09:27 +0300

    feat: add basic validation for Account service

--- project/libs/account/authentication/src/dto/signin-user.dto.ts
+++ project/libs/account/authentication/src/dto/signin-user.dto.ts
@@ -1,15 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
+import { AuthenticationValidateMessage } from '../authentication-module/authentication.constant';
 
 export class SignInUserDto {
   @ApiProperty({
     description: 'User unique email address',
     example: 'user@gmail.com',
   })
+  @IsEmail({}, { message: AuthenticationValidateMessage.EmailNotValid })
   public email: string;
 
   @ApiProperty({
     description: 'User password',
     example: '123456',
   })
+  @IsString()
   public password: string;
 }
--- project/libs/account/authentication/src/dto/signup-user.dto.ts
+++ project/libs/account/authentication/src/dto/signup-user.dto.ts
@@ -1,33 +1,41 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsISO8601, IsString } from 'class-validator';
+
+import { AuthenticationValidateMessage } from '../authentication-module/authentication.constant';
 
 export class SignUpUserDto {
   @ApiProperty({
     description: 'User unique email address',
     example: 'user@gmail.com',
   })
+  @IsEmail({}, { message: AuthenticationValidateMessage.EmailNotValid })
   public email: string;
 
   @ApiProperty({
     description: 'User birth date',
     example: '1981-03-12',
   })
+  @IsISO8601({}, { message: AuthenticationValidateMessage.DateBirthNotValid })
   public dateBirth: string;
 
   @ApiProperty({
     description: 'User first name',
     example: 'Cristiano',
   })
+  @IsString()
   public firstname: string;
 
   @ApiProperty({
     description: 'User last name',
     example: 'Ronaldo',
   })
+  @IsString()
   public lastname: string;
 
   @ApiProperty({
     description: 'User unique password',
     example: '123abc!*',
   })
+  @IsString()
   public password: string;
 }
