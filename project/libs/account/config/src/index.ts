8f6cfeecf3c879b96ff2f61ed0b77b085c0ea5b6
Anton Paskanny <anton.paskanny@gmail.com>
2024-04-29 09:47:24 +0300

    feat: agg Jwt module and jwt options

--- project/libs/account/config/src/index.ts
+++ project/libs/account/config/src/index.ts
@@ -4,4 +4,6 @@ export { default as applicationConfig } from './configurations/app.config';
 export { default as dbConfig } from './configurations/mongo.config';
 export { default as jwtConfig } from './configurations/jwt.config';
 
+export { getJwtOptions } from './get-jwt-options';
+
 export { getMongooseOptions } from './configurations/mongodb/get-mongoose-options';
