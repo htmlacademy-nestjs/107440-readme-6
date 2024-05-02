1fd97cc7efe61b6a16bf434d062138ab1551128e
Anton Paskanny <anton.paskanny@gmail.com>
2024-05-02 17:35:38 +0300

    feat: add static server module

--- project/apps/file-storage/webpack.config.js
+++ project/apps/file-storage/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/media'),
+    path: join(__dirname, '../../dist/apps/file-storage'),
   },
   plugins: [
     new NxWebpackPlugin({
