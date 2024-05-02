22be5e0f95f15cba0d80a0db63b7e5e5d01d8392
Anton Paskanny <anton.paskanny@gmail.com>
2024-05-02 20:08:58 +0300

    feat: update File Uploader module

--- project/libs/file-storage/file-uploader/src/file-uploader-module/file-uploader.controller.ts
+++ project/libs/file-storage/file-uploader/src/file-uploader-module/file-uploader.controller.ts
@@ -2,13 +2,17 @@ import 'multer';
 import { Express } from 'express';
 import {
   Controller,
+  Get,
+  Param,
   Post,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-
+import { MongoIdValidationPipe } from '@project/pipes';
 import { FileUploaderService } from './file-uploader.service';
+import { UploadedFileRdo } from '../rdo/uploaded-file.rdo';
+import { fillDto } from '@project/helpers';
 
 @Controller('files')
 export class FileUploaderController {
@@ -17,6 +21,13 @@ export class FileUploaderController {
   @Post('/upload')
   @UseInterceptors(FileInterceptor('file'))
   public async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    return this.fileUploaderService.saveFile(file);
+    const fileEntity = await this.fileUploaderService.saveFile(file);
+    return fillDto(UploadedFileRdo, fileEntity.toPOJO());
+  }
+
+  @Get(':fileId')
+  public async show(@Param('fileId', MongoIdValidationPipe) fileId: string) {
+    const existFile = await this.fileUploaderService.getFile(fileId);
+    return fillDto(UploadedFileRdo, existFile);
   }
 }
--- project/libs/file-storage/file-uploader/src/file-uploader-module/file-uploader.module.ts
+++ project/libs/file-storage/file-uploader/src/file-uploader-module/file-uploader.module.ts
@@ -1,9 +1,14 @@
 import { ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { FileUploaderService } from './file-uploader.service';
 import { FileUploaderController } from './file-uploader.controller';
+
+import { FileUploaderFactory } from './file-uploader.factory';
+import { FileModel, FileSchema } from './file.model';
+import { FileUploaderRepository } from './file-uploader.repository';
 
 const SERVE_ROOT = '/static';
 
@@ -27,8 +32,9 @@ const SERVE_ROOT = '/static';
         ];
       },
     }),
+    MongooseModule.forFeature([{ name: FileModel.name, schema: FileSchema }]),
   ],
-  providers: [FileUploaderService],
+  providers: [FileUploaderService, FileUploaderRepository, FileUploaderFactory],
   controllers: [FileUploaderController],
 })
 export class FileUploaderModule {}
