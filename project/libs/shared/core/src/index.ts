5d71edd83449766bfa9cf2899ad11393bd814cad
Anton Paskanny <anton.paskanny@gmail.com>
2024-05-03 21:21:17 +0300

    feat: add Rabbit MQ helpers

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -36,3 +36,5 @@ export { File } from './lib/types/file.interface';
 export { StoredFile } from './lib/types/stored-file.interface';
 
 export { Subscriber } from './lib/types/subscriber.interface';
+
+export { RabbitRouting } from './lib/types/rabbit-routing.enum';
