99a3c696b01f13e53dd5de70ac1aaf0f35164d02
Anton Paskanny <anton.paskanny@gmail.com>
2024-05-04 23:09:53 +0300

    feat: add HttpModule for Api

--- project/apps/api/src/app/app.module.ts
+++ project/apps/api/src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 
+import { HttpModule } from '@nestjs/axios';
+
+import { HTTP_CLIENT_MAX_REDIRECTS, HTTP_CLIENT_TIMEOUT } from './app.config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule.register({
+      timeout: HTTP_CLIENT_TIMEOUT,
+      maxRedirects: HTTP_CLIENT_MAX_REDIRECTS,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
